cb338c66b6b569ae43b1d118fc50407bfb1ad4fa
rifkyaris99 <rifky.aris@gmail.com>
2024-12-13 06:07:34 +0700

    Update appConfig.js

--- appConfig.js
+++ appConfig.js
@@ -4,7 +4,7 @@ module.exports = {
   description: "Discover step-by-step guides on how to play games effectively at serenityflow.web.id. Find valuable tips and strategies to enhance your gaming experience.",
   keywords: "how to, how to play, how to play games",
   author: "serenityflow.web.id",
-  favicon: "https://pugjs.org/images/favicon.ico",
+  favicon: "https://cdn1.iconfinder.com/data/icons/metro-ui-dock-icon-set--icons-by-dakirby/512/Google_Orkut.png",
   imageCount: 20,
   removeBadWords: "porn,nude,sex",
 };
