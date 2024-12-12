4bc4ae1723cde8cfd3f3efbeaa45a0a3894ae26b
rifkyaris99 <rifky.aris@gmail.com>
2024-12-13 05:32:32 +0700

    Update appConfig.js

--- appConfig.js
+++ appConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
   title: "How to play games - serenityflow.web.id",
   tagline: "Only How to play games - serenityflow.web.id  Has The Answer.",
-  description: "description" content="Discover step-by-step guides on how to play games effectively at serenityflow.web.id. Find valuable tips and strategies to enhance your gaming experience.",
+  description: "Discover step-by-step guides on how to play games effectively at serenityflow.web.id. Find valuable tips and strategies to enhance your gaming experience.",
   keywords: "how to, how to play, how to play games",
   author: "serenityflow.web.id",
   favicon: "https://pugjs.org/images/favicon.ico",
